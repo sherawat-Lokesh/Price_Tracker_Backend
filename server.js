6d2924b824e8ddd3d424a459c1f137b3cbda82df
SHERAWAT LOKESH <107927380+sherawat-Lokesh@users.noreply.github.com>
2023-02-02 12:46:11 -0800

    fixed errors

--- server.js
+++ server.js
@@ -9,12 +9,11 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const obj = {
-  amazon:
-    "#corePriceDisplay_desktop_feature_div > div.a-section.a-spacing-none.aok-align-center > span.a-price.aok-align-center.reinventPricePriceToPayMargin.priceToPay > span:nth-child(2) > span.a-price-whole",
+  amazon:'.priceToPay > span:nth-child(2) > span.a-price-whole',
   amazonImage: "#landingImage",
   flipkart:
-    "#container > div > div._2c7YLP.UtUXW0._6t1WkM._3HqJxg > div._1YokD2._2GoDe3 > div._1YokD2._3Mn1Gg.col-8-12 > div:nth-child(2) > div > div.dyC4hf > div.CEmiEU > div > div._30jeq3._16Jk6d",
-  flipkartImage: "div._1BweB8 > div._3kidJX > div.CXW8mj._3nMexc > img",
+    "div._30jeq3._16Jk6d",
+  flipkartImage: "._3nMexc > img",
 };
 
 app.post("/info", (req, res) => {
